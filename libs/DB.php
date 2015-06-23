2faa41d941c09e59ebee0b304cae0f00f91bfd3f
asrulhadi <git@asrulhadi.net>
2015-06-23 11:56:57 +0800

    Retab

--- libs/DB.php
+++ libs/DB.php
@@ -13,49 +13,49 @@
  */
 
 class DB {
-	/**
-	 * This class provides the barest wrapper for MySQL
-	 */
-	private $host = "localhost";
-	private $username = "root";
-	private $password = "toor";
-	private $db_name = "insecure";
-	private static $instance;
+  /**
+   * This class provides the barest wrapper for MySQL
+   */
+  private $host = "localhost";
+  private $username = "root";
+  private $password = "toor";
+  private $db_name = "insecure";
+  private static $instance;
 
-	private function __construct() {
-		$this->connect();
-	}
+  private function __construct() {
+    $this->connect();
+  }
 
-	function connect() {
-		$this->connection = mysqli_connect($this->host, $this->username, $this->password);
-		if(!$this->connection) {
-			echo(mysqli_error());
-		}
-		mysqli_select_db($this->connection, $this->db_name);
-	}
+  function connect() {
+    $this->connection = mysqli_connect($this->host, $this->username, $this->password);
+    if(!$this->connection) {
+      echo(mysqli_error());
+    }
+    mysqli_select_db($this->connection, $this->db_name);
+  }
 
-	function query($sql) {
-		$result = mysqli_query($this->connection, $sql);
-		if(!$result) {
-			echo("Function query: " . mysqli_error($this->connection));
-		}
-		return $result;
-	}
+  function query($sql) {
+    $result = mysqli_query($this->connection, $sql);
+    if(!$result) {
+      echo("Function query: " . mysqli_error($this->connection));
+    }
+    return $result;
+  }
 
-	function count_rows($result) {
-		return mysqli_num_rows($result);
-	}
+  function count_rows($result) {
+    return mysqli_num_rows($result);
+  }
 
-	function fetch_assoc($result) {
-		return mysqli_fetch_assoc($result);
-	}
+  function fetch_assoc($result) {
+    return mysqli_fetch_assoc($result);
+  }
 
-	static function get_instance() {
-		if(!self::$instance) {
-			self::$instance = new DB;
-		}
-		return self::$instance;
-	}
+  static function get_instance() {
+    if(!self::$instance) {
+      self::$instance = new DB;
+    }
+    return self::$instance;
+  }
 
 }
 
--- libs/User.php
+++ libs/User.php
@@ -13,64 +13,64 @@
  */
 
 class User {
-	private $username = null;
-	private $password = null;
-	private $user_id = null;
+  private $username = null;
+  private $password = null;
+  private $user_id = null;
 
-	function __construct($user_id='') {
-		if($this->check_user_session()) {
-			$this->set_username($_SESSION['username']);
-			$this->set_user_id($_SESSION['user_id']);
-		}
-	}
+  function __construct($user_id='') {
+    if($this->check_user_session()) {
+      $this->set_username($_SESSION['username']);
+      $this->set_user_id($_SESSION['user_id']);
+    }
+  }
 
-	function get_user_id() {
-		return $this->user_id;
-	}
+  function get_user_id() {
+    return $this->user_id;
+  }
 
-	function set_user_id($user_id) {
-		$this->user_id = $user_id;
-	}
+  function set_user_id($user_id) {
+    $this->user_id = $user_id;
+  }
 
-	function get_username() {
-		return $this->username;
-	}
+  function get_username() {
+    return $this->username;
+  }
 
-	function set_username($username) {
-		$this->username = $username;
-	}
+  function set_username($username) {
+    $this->username = $username;
+  }
 
-	/**
-	 * This function will check a user submitted username and password against 
-	 * the database. If it exists, it sets up a new session
-	 */
-	function login($username, $password) {
-		$db = DB::get_instance();
-		$sql = "select * from user where username = '$username' and password = $password";
-		$result= $db->query($sql);
-		if($db->count_rows($result)) {
-			$row = $db->fetch_assoc($result);
-			$this->set_user_id($row['id']);
-			$this->set_username($row['username']);
-			$this->create_user_session();
-		}
-	}
+  /**
+   * This function will check a user submitted username and password against 
+   * the database. If it exists, it sets up a new session
+   */
+  function login($username, $password) {
+    $db = DB::get_instance();
+    $sql = "select * from user where username = '$username' and password = $password";
+    $result= $db->query($sql);
+    if($db->count_rows($result)) {
+      $row = $db->fetch_assoc($result);
+      $this->set_user_id($row['id']);
+      $this->set_username($row['username']);
+      $this->create_user_session();
+    }
+  }
 
-	// This function is used to start a session and set initial variables
-	private function create_user_session() {
-		session_start();
-		$_SESSION['user_id'] = $this->get_user_id();
-		$_SESSION['username'] = $this->get_username();
-	}
+  // This function is used to start a session and set initial variables
+  private function create_user_session() {
+    session_start();
+    $_SESSION['user_id'] = $this->get_user_id();
+    $_SESSION['username'] = $this->get_username();
+  }
 
-	// This function checks to see if a user is logged in.
-	function check_user_session() {
-		session_start();
-		if($_SESSION['user_id'] && $_SESSION['username']) {
-			return true;
-		}
-		return false;
-	}
+  // This function checks to see if a user is logged in.
+  function check_user_session() {
+    session_start();
+    if($_SESSION['user_id'] && $_SESSION['username']) {
+      return true;
+    }
+    return false;
+  }
 }
 
 // vim: et:sta:ai:ts=2:sw=2:fen:fdm=indent:
